ccae0ee70570a4cdb2e6244111474975f09d8a74
Naplesoul <shenwhang@outlook.com>
2021-03-08 14:49:42 +0800

    create static frame

--- bookstore-front/src/App.vue
+++ bookstore-front/src/App.vue
@@ -1,8 +1,9 @@
 <template>
+  <body>
   <div id="container">
     <div id="header">
       <div class="logo">
-        <img alt="logo" src="./assets/logo.png" height="50" width="50">
+        <img alt="logo" src="./assets/logo.png" height="100%">
       </div>
       <div class="title">
         <h1>eBookstore</h1>
@@ -11,12 +12,23 @@
         <h3>swh</h3>
       </div>
       <div class="userimage">
-        <img alt="logo" src="./assets/logo.png" height="30" width="30">
+        <img alt="logo" src="./assets/logo.png" height="100%">
       </div>
     </div>
     <div id="content">
       <div id="sider">
-
+        <div class="sidebar_chosen">
+          <h3>Books</h3>
+        </div>
+        <div class="sidebar">
+          <h3>Cart</h3>
+        </div>
+        <div class="sidebar">
+          <h3>My Orders</h3>
+        </div>
+        <div class="sidebar">
+          <h3>My Profile</h3>
+        </div>
       </div>
       <div id="app">
         <HomePage/>
@@ -24,6 +36,8 @@
     </div>
 
   </div>
+  </body>
+
 </template>
 
 <script>
@@ -45,72 +59,131 @@ export default {
 
 #header
 {
-  float: top;
-  height: 80px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 8%;
   background: #2c3e50;
 }
 
 #header>div.logo
 {
-  height: 50px;
+  height: 70%;
   float: left;
-  margin-top: 15px;
-  margin-left: 30px;
+  margin-left: 2%;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
 }
 
 #header>div.title
 {
-  height: 50px;
+  height: 50%;
   float: left;
-  margin-top: 15px;
-  margin-left: 30px;
+  margin-left: 1%;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
 }
 
 #header>div.title>h1
 {
-  size: 50px;
-  margin-top: 0px;
+  font-size: 3vh;
+  margin: 0;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
   color: white;
 }
 
 #header>div.username
 {
+  height: 50%;
+  width: 5%;
   float: right;
-  margin-right: 30px;
-  margin-top: 25px;
-  height: 30px;
+  margin-right: 2%;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
 }
 
 #header>div.username>h3
 {
-  size: 30px;
-  margin-top: 0px;
+  font-size: 2vh;
+  margin: 0;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
   color: white;
 }
 
 #header>div.userimage
 {
-  height: 30px;
-  width: 30px;
+  height: 40%;
   float: right;
-  margin-right: 10px;
-  margin-top: 25px;
-  /*background: cyan;*/
+  margin-right: 1%;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
 }
 
 #content
 {
-  height: 720px;
+  position: fixed;
+  top: 8%;
+  height: 92%;
+  width: 100%;
   /*background: #9900CC;*/
 }
 
 #sider
 {
-  height: 715px;
-  width: 200px;
-  margin-top: 5px;
+  height: 100%;
+  width: 12%;
+  margin-top: 0;
   float: left;
-  background: aqua;
+  background: white;
+  border-right: 1px solid #e8e8e8;
+  /*opacity: 0.8;*/
+}
+
+#sider>div.sidebar_chosen
+{
+  /*position: absolute;*/
+  /*top: 0;*/
+  float: top;
+  height: 7%;
+  /*width: 10%;*/
+  border-right:6px solid #2196F3;
+  background: #ddffff;
+}
+
+#sider>div.sidebar_chosen>h3
+{
+  text-align: center;
+  font-size: 2vh;
+  margin: 0;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  color: black;
+}
+
+#sider>div.sidebar
+{
+  float: top;
+  height: 7%;
+}
+
+#sider>div.sidebar>h3
+{
+  text-align: center;
+  font-size: 2vh;
+  margin: 0;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  color: black;
 }
 
 #app {
@@ -119,6 +192,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 0px;
+  margin-top: 0;
+}
+
+body
+{
+  margin: 0;
 }
 </style>
